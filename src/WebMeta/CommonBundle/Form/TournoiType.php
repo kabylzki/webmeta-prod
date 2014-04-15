23bb0c6fa6120d2436d9b04fb39e55ff30bb1889
efiareta <lextra58@gmail.com>
2014-04-15 18:50:12 +0400

    efia ajout formulaire tournoi

---
+++ src/WebMeta/CommonBundle/Form/TournoiType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace WebMeta\CommonBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TournoiType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('status')
+            ->add('date')
+            ->add('valider', 'submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'WebMeta\CommonBundle\Entity\Tournoi'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'webmeta_commonbundle_tournoi';
+    }
+}
