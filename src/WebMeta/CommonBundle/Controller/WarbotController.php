349f642356b692cf998252d9c378ce25781b3e21
efiareta <lextra58@gmail.com>
2014-04-19 18:11:13 +0400

    efia ajout formulaire tournoi, match

--- src/WebMeta/CommonBundle/Controller/WarbotController.php
+++ src/WebMeta/CommonBundle/Controller/WarbotController.php
@@ -3,6 +3,9 @@
 namespace WebMeta\CommonBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use WebMeta\CommonBundle\Entity\Tournoi;
+use WebMeta\CommonBundle\Form\TournoiType;
+use Symfony\Component\HttpFoundation\Request;
 
 class WarbotController extends Controller
 {
@@ -27,5 +30,39 @@ class WarbotController extends Controller
     public function tournoiAction()
     {
         return $this->render('WebMetaCommonBundle:Warbot:tournoi.html.twig');
+    }
+
+    public function creationTournoiAction() {
+        $tournoi = new Tournoi();
+        $form = $this->createForm(new TournoiType(), $tournoi);
+
+        return $this->render('WebMetaCommonBundle:Warbot:creation.html.twig', array('form'=>$form->createView()));
+    }
+
+    public function validationAction(Request $request) {
+        $tournoi = new Tournoi();
+
+        $form = $this->createForm(new TournoiType(), $tournoi);
+
+        $form->handleRequest($request);
+
+        // Si le formulaire est valide alors on insert
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($tournoi);
+            $em->flush();
+
+            $message = "Votre tournoi a été crée avec succès";
+        } else {
+            $message = "Une erreur est survenue, création impossible";
+        }
+
+        // Message de confirmation pour l'utilisateur
+        $this->get('session')->getFlashBag()->add(
+            'notice',
+            $message
+        );
+
+        return $this->redirect($this->generateUrl('warbot_tournoi'));
     }
 }
